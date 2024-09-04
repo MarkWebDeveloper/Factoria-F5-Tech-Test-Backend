6a6fb0ff26f69d5d5aab3e400130e5cb16bd4efb
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-09-04 12:21:19 +0200

    first commit

---
+++ src/main/java/dev/mark/factoria_tech_test/FactoriaTechTestApplication.java
@@ -0,0 +1,13 @@
+package dev.mark.factoria_tech_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoriaTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoriaTechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mark/factoria_tech_test/FactoriaTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.factoria_tech_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoriaTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
