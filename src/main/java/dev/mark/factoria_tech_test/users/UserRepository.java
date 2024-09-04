cc9730225b4d556e9062fa16cda2f8af94e9f693
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-09-04 17:16:53 +0200

    auth controller and security user are added

--- src/main/java/dev/mark/factoria_tech_test/users/UserRepository.java
+++ src/main/java/dev/mark/factoria_tech_test/users/UserRepository.java
@@ -1,10 +1,12 @@
 package dev.mark.factoria_tech_test.users;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username); 
     default List<User> findAll() {
         throw new UnsupportedOperationException("Unsupported operation. Please, use findById or findByEmail instead");
     }
