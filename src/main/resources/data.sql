110b52394b5e64ab9cffdb5fa8b19db777c4d35d
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-09-06 18:57:32 +0200

    default images are added to user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,3 +13,25 @@ INSERT INTO role_users (role_id, user_id) VALUES (2, 2);
 /*Profile default*/
 INSERT INTO profiles(id_profile) VALUES(default);
 INSERT INTO profiles(id_profile) VALUES(default);
+
+ /*Images*/
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'beach.jpg', 'Florida Beach');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'turtle.jpg', 'Big Turtle Under the Sea');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'big-tree.jpg', 'Giant Old Tree');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'mountains-1.jpg', 'Snowy Japanese Mountains');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'road.jpg', 'Highway to Paradice');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'mountains-2.jpg', 'Serenity and Peace');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'stars.jpg', 'Endless Space');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'forest-2.jpg', 'The Autumn Leaves');
+ INSERT INTO images(id_image, image_name, image_title) VALUES(default, 'mountains-3.jpg', 'Just Green and Beautiful');
+
+  /*Images-Profiles*/
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 1);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 2);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 3);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 4);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 5);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 6);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 7);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 8);
+  INSERT INTO images_profiles (profile_id, image_id) VALUES (2, 9);
